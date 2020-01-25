1675dbd72b9d313c3727836067a99314af04726f
Keith <ozmaneku@gmail.com>
2020-01-25 12:24:54 -0500

    Added in About Me adjustment.

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -5,7 +5,12 @@ class aboutme extends Component {
         return (
             <div id='aboutPage'>
                 <div className='aboutParallax'>
-                    <h1 className='aboutText'>ABOUT ME TEST</h1>
+                    <div className='aboutText'>
+                    <h1>ABOUT ME</h1>
+                    <p>Howdy folks, my name is Max but people on the internet call me Manny!</p>
+                    <p>I've been streaming on twitch since 2013 and I'm a professional Apex Legends player/streamer.</p>
+                    <p>I'm a cyber security professional outside of streaming. I enjoy making peoples days and making people smile with my content!</p>
+                    </div>
                 </div>
             </div>
         );
