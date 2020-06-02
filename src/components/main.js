b1616a13982386cb8ed545df658581ae83d020a4
Keith <ozmaneku@gmail.com>
2020-06-02 10:54:59 -0400

    Switching back to animejs standard

--- src/components/main.js
+++ src/components/main.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
-import Anime from 'react-anime'
 
 class main extends Component {
     render() {
         return (
-                <Anime
-                translateX={[500]}
-                delay={[1000]}
-                easing={['easeInOutQuad']}
-                direction={['reverse']}
-                loop={[3]}>
-                    <div className='test'></div>
-                </Anime>
+                <div>
+                    <h1>Manny</h1>
+                </div>
         );
     }
 }
