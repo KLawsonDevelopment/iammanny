92def630e8fe8a5f1c40f9c2d43643c7bf9b6ee3
Keith <ozmaneku@gmail.com>
2020-06-02 09:26:58 -0400

    Restarting fresh in the project

--- src/App.js
+++ src/App.js
@@ -2,31 +2,14 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Main from './components/main';
-import AboutMe from './components/aboutme';
-import Live from './components/live';
-import Socials from './components/socials'
-import Menu from './components/menu'
+
 
 function App() {
   return (
 
     // Initial router setup to controll what order the Components go into.
     <Router>
-
-      {/* Menu icon to control the dropdown to skip to certain parts of the site. */}
-      <Menu></Menu>
-
-      {/* Initial main page, will show a clip of twitch streamer. */}
       <Main></Main>
-
-      {/* About Me page for the streamer. Short biography to explain their story and what they want to do on twitch. */}
-      <AboutMe></AboutMe>
-
-      {/* Live page. Pull a direct link to the twitch page of the streamer, and make it a specific size. */}
-      <Live></Live>
-
-      {/* Basic social links page. Will show Twitter, Instagram, and any other social platforms the streamer is on via clickable icons. */}
-      <Socials></Socials>
     </Router>
   );
 }
