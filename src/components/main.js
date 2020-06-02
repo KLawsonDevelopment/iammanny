b31371de327f5bd9007aa608d290311d15cb110d
Keith <ozmaneku@gmail.com>
2020-06-02 11:30:41 -0400

    Attempting react anime, documentation found to help with development process.

--- src/components/main.js
+++ src/components/main.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
+import anime from 'animejs'
 
 class main extends Component {
+
     render() {
         return (
-                <div>
-                    <h1>Manny</h1>
-                </div>
-        );
+        <div>Hello World</div>
+        )
     }
 }
 
