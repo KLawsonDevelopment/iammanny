803979df17d7da97fe27b0cf3ca156252ab47671
Keith <ozmaneku@gmail.com>
2020-06-04 08:54:58 -0400

    After figuring out animejs, the project officially restarts.

--- src/components/main.js
+++ src/components/main.js
@@ -1,28 +1,10 @@
 import React, { Component } from 'react';
-import anime from 'animejs'
 
 class main extends Component {
-
-    openingScreen() {
-        anime({
-            targets: '.test',
-            width: '99%',
-            direction: 'alternate',
-            easing: 'spring',
-            delay: 1000
-        })
-    }
-
-    componentDidMount() {
-        this.openingScreen()
-    }
-    
     render() {
         return (
             <div>
-                <h1>Hello World</h1>
-                <div className='test'>
-                </div>
+                Hello
             </div>
         );
     }
