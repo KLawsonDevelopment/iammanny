bc069248cef6ea526c4760932defad04242ce7c8
Keith <ozmaneku@gmail.com>
2020-06-11 10:44:23 -0400

    Created basic animations for showing and removing.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,20 +3,39 @@ import anime from 'animejs'
 
 class navbar extends Component {
 
-    testFunction () {
-        anime ({
+    testFunction() {
+        anime({
             targets: '.animationPiece',
-            width: '100%',
+            width: [ '1', '100%'],
             direction: 'alternate',
-            easing: 'easeInOutQuad'
+            easing: 'easeInOutQuad',
+            endDelay: 500
         })
+        setTimeout(function () {
+            var visible = document.getElementById('centerPage')
+            visible.classList.add('showClass')
+        }, 1000)
+    }
+
+    testFunction2() {
+        anime({
+            targets: '.animationPiece',
+            width: [ '1', '100%'],
+            direction: 'alternate',
+            easing: 'easeInOutQuad',
+            endDelay: 500
+        })
+        setTimeout(function () {
+            var invisible = document.getElementById('centerPage')
+            invisible.classList.remove('showClass')
+        }, 1000)
     }
 
     render() {
         return (
             <div className='navBar'>
                 <button className='mainLink' onClick={this.testFunction}>Main</button>
-                <button className='aboutLink' >About Me</button>
+                <button className='aboutLink' onClick={this.testFunction2}>About Me</button>
                 <button className='twitchLink'>Twitch</button>
                 <button className='socialLink'>Social Media</button>
             </div>
