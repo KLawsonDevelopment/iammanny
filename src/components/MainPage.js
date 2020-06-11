172739119ccbc8402a75f5212a1c3ce806f64a4a
Keith <ozmaneku@gmail.com>
2020-06-11 13:10:11 -0400

    Created animation transition for the 4 main pages. Now working on content.

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -6,7 +6,9 @@ class MainPage extends Component {
             <div className='mainBox'>
                 <div className='animationPiece'></div>
                 <h1 className='mainPage' id='mainPage'>I Am Manny</h1>
-                <div className='closingPiece'></div>
+                <h1 className='aboutPage' id='aboutPage'>About Me</h1>
+                <h1 className='twitchPage' id='twitchPage'>Twitch</h1>
+                <h1 className='socialPage' id='socialPage'>Socials</h1>
             </div>
         );
     }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,33 +3,139 @@ import anime from 'animejs'
 
 class navbar extends Component {
 
-    openingWebsite () {
+    openingWebsite = () => {
         setTimeout(function () {
             anime({
                 targets: '.animationPiece',
-                width: [ 0, '100%'],
+                width: [0, '100%'],
                 direction: 'alternate',
                 easing: 'easeInOutQuad',
-                endDelay:500
+                endDelay: 500
             })
-        },750)
+        }, 750)
         setTimeout(function () {
             var visible = document.getElementById('mainPage')
             visible.classList.add('showClass')
         }, 2000)
     }
 
-    componentDidMount () {
+    componentDidMount() {
         this.openingWebsite()
+    }
+
+
+
+    showMain = () => {
+        anime({
+            targets: '.animationPiece',
+            width: [0, '100%'],
+            direction: 'alternate',
+            easing: 'easeInOutQuad',
+            endDelay: 500
+        })
+        setTimeout(function () {
+            var main = document.getElementById('mainPage');
+            var about = document.getElementById('aboutPage');
+            var twitch = document.getElementById('twitchPage');
+            var social = document.getElementById('socialPage');
+            main.classList.remove('showClass');
+            about.classList.remove('showClass');
+            twitch.classList.remove('showClass');
+            social.classList.remove('showClass');
+            var visible = document.getElementById('mainPage')
+            visible.classList.add('showClass')
+        }, 1000)
+    }
+
+    showAbout = () => {
+        anime({
+            targets: '.animationPiece',
+            width: [0, '100%'],
+            direction: 'alternate',
+            easing: 'easeInOutQuad',
+            endDelay: 500
+        })
+        setTimeout(function () {
+            var main = document.getElementById('mainPage');
+            var about = document.getElementById('aboutPage');
+            var twitch = document.getElementById('twitchPage');
+            var social = document.getElementById('socialPage');
+            main.classList.remove('showClass');
+            about.classList.remove('showClass');
+            twitch.classList.remove('showClass');
+            social.classList.remove('showClass');
+            var visible = document.getElementById('aboutPage')
+            visible.classList.add('showClass')
+        }, 1000)
+    }
+
+    showTwitch = () => {
+        anime({
+            targets: '.animationPiece',
+            width: [0, '100%'],
+            direction: 'alternate',
+            easing: 'easeInOutQuad',
+            endDelay: 500
+        })
+        setTimeout(function () {
+            var main = document.getElementById('mainPage');
+            var about = document.getElementById('aboutPage');
+            var twitch = document.getElementById('twitchPage');
+            var social = document.getElementById('socialPage');
+            main.classList.remove('showClass');
+            about.classList.remove('showClass');
+            twitch.classList.remove('showClass');
+            social.classList.remove('showClass');
+            var visible = document.getElementById('twitchPage')
+            visible.classList.add('showClass')
+        }, 1000)
+    }
+
+    showSocial = () => {
+        anime({
+            targets: '.animationPiece',
+            width: [0, '100%'],
+            direction: 'alternate',
+            easing: 'easeInOutQuad',
+            endDelay: 500
+        })
+        setTimeout(function () {
+            var main = document.getElementById('mainPage');
+            var about = document.getElementById('aboutPage');
+            var twitch = document.getElementById('twitchPage');
+            var social = document.getElementById('socialPage');
+            main.classList.remove('showClass');
+            about.classList.remove('showClass');
+            twitch.classList.remove('showClass');
+            social.classList.remove('showClass');
+            var visible = document.getElementById('socialPage')
+            visible.classList.add('showClass')
+        }, 1000)
+    }
+
+    mainClick = () => {
+        this.showMain()
+    }
+
+    aboutClick = () => {
+        this.showAbout()
+    }
+
+    twitchClick = () => {
+        this.showTwitch()
+    }
+
+    socialClick = () => {
+        this.showSocial()
     }
 
     render() {
         return (
             <div className='navBar'>
-                <button className='mainLink'>Main</button>
-                <button className='aboutLink'>About Me</button>
-                <button className='twitchLink'>Twitch</button>
-                <button className='socialLink'>Social Media</button>
+                <button className='mainLink' onClick={this.mainClick}>Main</button>
+                <button className='aboutLink' onClick={this.aboutClick}>About Me</button>
+                <button className='twitchLink' onClick={this.twitchClick}>Twitch</button>
+                <button className='socialLink' onClick={this.socialClick}>Social Media</button>
             </div>
         );
     }
