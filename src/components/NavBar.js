fba27b24f9b007510a170ed15ebf3564392883fb
Keith <ozmaneku@gmail.com>
2020-06-11 12:21:46 -0400

    Figured out how to shift the animation over to make it smoother.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ import anime from 'animejs'
 
 class navbar extends Component {
 
-    componentDidMount () {
+    openingWebsite () {
         setTimeout(function () {
             anime({
                 targets: '.animationPiece',
@@ -19,25 +19,15 @@ class navbar extends Component {
         }, 2000)
     }
 
-    testFunction2() {
-        anime({
-            targets: '.animationPiece',
-            width: [ 0, '100%'],
-            direction: 'alternate',
-            easing: 'easeInOutQuad',
-            endDelay: 500
-        })
-        setTimeout(function () {
-            var invisible = document.getElementById('mainPage')
-            invisible.classList.remove('showClass')
-        }, 1000)
+    componentDidMount () {
+        this.openingWebsite()
     }
 
     render() {
         return (
             <div className='navBar'>
                 <button className='mainLink'>Main</button>
-                <button className='aboutLink' onClick={this.testFunction2}>About Me</button>
+                <button className='aboutLink'>About Me</button>
                 <button className='twitchLink'>Twitch</button>
                 <button className='socialLink'>Social Media</button>
             </div>
