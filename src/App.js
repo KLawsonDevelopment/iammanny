723fd671d83ec16b8155b5b28d9ab2d4c1223683
Keith <ozmaneku@gmail.com>
2020-06-11 09:33:50 -0400

    Created second part of page, center based grid/flexbox for all content in the center of the screen.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavBar from './components/NavBar'
+import MainPage from './components/MainPage';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     // Initial router setup to controll what order the Components go into.
     <Router>
       <NavBar></NavBar>
+      <MainPage></MainPage>
     </Router>
   );
 }
---
+++ src/components/MainPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MainPage extends Component {
+    render() {
+        return (
+            <div className='mainBox'>
+                <div className='centerPage'>Hello World</div>
+            </div>
+        );
+    }
+}
+
+export default MainPage;
